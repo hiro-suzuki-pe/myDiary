a7669d587ae852756bb10fa537ecaeb37e1d7fce
Hiroyuki Suzuki <suzuki.hiroyuki@mbr.nifty.com>
2023-02-22 04:45:33 +0900

    2022/1/1のデータの操作

--- menu.py
+++ menu.py
@@ -3,14 +3,68 @@ import tkinter as tk
 from tkinter import messagebox
 
 def save():
-    messagebox.showinfo('メッセージ', '保存メニューです。')
+    con = sqlite3.connect('diary.db', isolation_level=None,)
+    cur = con.cursor()
+    r_data = ['2022_1_1', 2, 80, 4, """あけまして、おめでとう。
+目が覚めたら10時だった。
+今年こそは、ダイエットすると決めていたのに、
+結局だらだら1日が過ぎてしまった。"""]
+    sql = "REPLACE INTO diary VALUES (?,?,?,?,?)"
+    cur.execute(sql, r_data)
+    con.commit()
+    con.close()
+    messagebox.showinfo('メッセージ', '"2022_1_1のデータを保存しました。')
+    
     
 def delete():
-    messagebox.showinfo('メッセージ', '削除メニューです。')
+    con = sqlite3.connect('diary.db')
+    cur = con.cursor()
+    sql = "DELETE FROM diary WHERE date='2022_1_1';"
+    cur.execute(sql)
+    con.commit()
+    con.close()
+    messagebox.showinfo('メッセージ', '"2022_1_1のデータを削除しました。')
     
 def search():
-    messagebox.showinfo('メッセージ', '検索メニューです。')
+
+#    messagebox.showinfo('メッセージ', '検索メニューです。')
+    global s_txt
+    global s_text
     
+    sub_window = tk.Toplevel()
+    sub_window.geometry('320x220')
+    sub_window.title('検索テスト')
+ 
+    s_txt = tk.Entry(sub_window, width=20)
+    s_txt.pack(pady=10)
+    
+    button = tk.Button(sub_window, text='検索', command=click_search)
+    button.pack()
+    
+    frame = tk.Frame(sub_window)
+    s_text = tk.Text(frame, width=40, height=10)
+    s_text.grid(row=0, column=0)
+    scroll_v = tk.Scrollbar(frame, orient=tk.VERTICAL, command=s_text.yview)
+    scroll_v.grid(row=0, column=1, sticky=tk.N+tk.S)
+    s_text["yscrollcommand"] = scroll_v.set
+    frame.pack(pady=10)
+    
+def click_search():
+    global s_txt
+    global s_text
+    
+    con = sqlite3.connect('diary.db')
+    cur = con.cursor()
+    sql = """SELECT * FROM diary WHERE event LIKE '%""" \
+        + s_txt.get() + """%' ORDER BY date DESC;"""
+    cur.execute(sql)
+    s_text.delete('1.0', 'end')
+    for row in cur:
+        s_text.insert('1.0', row[0] + '\n' + row[4] + '\n\n')
+    con.close()
+        
+        
+        
 root=tk.Tk()
 root.geometry('320x220')
 root.title('メニューテスト')
